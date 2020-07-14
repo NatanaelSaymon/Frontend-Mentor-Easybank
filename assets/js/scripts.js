459d5068fbfd8f3fe4936485de77e099b81daa4b
Natanael Saymon <nsaymonsouza@gmail.com>
2020-07-13 22:24:18 -0300

    Done: section hero concluida

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+let show = true
+const menuSection = document.querySelector('.menu-section')
+const menuToggle = document.querySelector('.menu-toggle')
+
+menuToggle.addEventListener("click", () =>{
+  // document.body.style.overflow = show ? "hidden" : "initial"
+  if(show == true){
+    document.body.style.overflow = "hidden"
+  }
+  else{
+    document.body.style.overflow = "initial"
+  }
+  menuSection.classList.toggle("on", show)
+  show = !show
+})
